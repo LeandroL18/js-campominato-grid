4caae48cb0d6d2e4a4e1f5b5cde9911f130abcca
LeandroL18 <leandrolipani6@gmail.com>
2023-11-29 15:09:01 +0100

    Esercizio completo

--- js/myscript.js
+++ js/myscript.js
@@ -3,3 +3,41 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
+// selezione griglia
+const grid = document.getElementById("grid");
+
+// selezione bottoni
+const buttonPlay = document.getElementById("play");
+
+
+// far apparire la griglia al click del bottone
+buttonPlay.addEventListener("click",
+    function(){
+        grid.style.display = "block";
+    }
+)
+
+// creazione celle
+for(let i = 1; i <= 100; i++){
+    const newElement = createMyElement("div","square");
+    grid.append(newElement);
+    newElement.append(i);
+
+    // colorare la cella al suo click
+    newElement.addEventListener("click",
+    function(){
+        newElement.classList.add("clicked");
+        console.log("Cella " + i);
+        }
+    )
+}
+
+
+
+
+// funzione per la creazione dell'elemento
+function createMyElement(tagtype, classname){
+    const currentElement = document.createElement(tagtype);
+    currentElement.classList.add(classname);
+    return currentElement;
+}
